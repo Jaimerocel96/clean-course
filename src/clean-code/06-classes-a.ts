947102fe777fbcef342fc74aa3a7b7c3792fb2de
Jaime Rodríguez <jaimerocel96@gmail.com>
2025-06-17 16:22:20 +0200

    Clean Code - Ejercicio 6 parte 2

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -12,8 +12,49 @@
         }
     }
 
-    const newPerson = new Person('Jaime', 'M', new Date('1996-01-04'));
+    class User extends Person {
+        public lastAccess: Date;
 
-    console.log({newPerson});
+        constructor(
+            public email:   string,
+            public role:    string,
+            name:           string,
+            gender:         Gender,
+            birthdate:      Date,
+        ){
+            super(name, gender, birthdate);
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials(){
+            return true;
+        }   
+    }
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory:    string,
+            public lastOpenFolder:      string,
+            email:                      string,
+            role:                       string,
+            name:                       string,
+            gender:                     Gender,
+            birthdate:                  Date,
+        ){
+            super(email, role, name, gender, birthdate);
+        }
+    }
+
+
+    const userSettings = new UserSettings(
+        '/usr/home',
+        '/home',
+        'test@test.com',
+        'Admin',
+        'Jaime',
+        'M',
+        new Date('1996-01-04'));
+    
+    console.log({ userSettings });
 
 })();
