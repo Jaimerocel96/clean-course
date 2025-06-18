66848753ede3f070abd49a648ff11d0104d61627
Jaime Rodríguez <jaimerocel96@gmail.com>
2025-06-18 14:27:38 +0200

    Clean Code - OPC parte 2

--- src/solid/02-open-close-c.ts
+++ src/solid/02-open-close-c.ts
@@ -1,10 +1,19 @@
-import axios from "axios";
+// import axios from "axios";
 
 export class HttpClient {
+    
+    // async get(url: string) {
+    //     const { data,  status } = await axios.get(url);
+    //     console.log({ data, status });
+    //     return { data, status };
+    // }
+
     async get(url: string) {
-        const { data,  status } = await axios.get(url);
-        console.log({ data, status });
-        return { data, status };
+    
+        const result = await fetch(url);
+        const data = await result.json();
+
+        return { data, status: result.status }
     }
 
 }
